4e20cd398ed1f0bcc28cbfdbb294719bcbebed4e
Ubuntu <ubuntu@ip-172-31-7-8.ec2.internal>
2021-09-28 00:20:48 +0000

    grandchild dating

---
+++ grandchild_dating.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 Michael Zagon All rights reserved
+
+// Created by: Michael Zagon
+// Created on: September 2021
+// This program is tells a user if they are well suited for my grandchild
+
+#include <iostream>
+
+int main() {
+    // This function determines if the user is a good fit for my grandchild
+
+    int user_age;
+    std::string user_age_string;
+
+    // Input
+    std::cout << "Enter your age: ";
+    std::cin >> user_age_string;
+    std::cout << "" << std::endl;
+
+    // Process and Output
+    try {
+        user_age = std::stoi(user_age_string);
+
+        if (user_age > 40) {
+            std::cout << "You are too old for my grandchild." << std::endl;
+        } else if (user_age < 25) {
+            std::cout << "You are too young for my grandchild." << std::endl;
+        } else {
+            std::cout << "You are well suited for my grandchild."
+            << std::endl; }
+    }    catch (std::invalid_argument) {
+             std::cout << "That was not a valid input." << std::endl;
+    }
+
+    std::cout << ("\nDone.") << std::endl;
+}
